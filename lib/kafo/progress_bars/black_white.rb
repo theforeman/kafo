36251275bf760eb39504ad14a6eb856ac9bdfd0c
Marek Hulan <mhulan@redhat.com>
2013-10-07 10:14:57 +0200

    Fixes #3062 - Add --no-colors support

---
+++ lib/kafo/progress_bars/black_white.rb
@@ -0,0 +1,15 @@
+module ProgressBars
+  class BlackWhite < ProgressBar
+
+    private
+
+    def finite_template
+      'Installing'.ljust(22) + ' ${<msg>} [${<percent>%}] [${<bar>}]'
+    end
+
+    def infinite_template
+      'Preparing installation ${<msg>}'
+    end
+
+  end
+end
