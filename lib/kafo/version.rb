356f2bf18f95b7a919c17929ceedb48ede073620
Marek Hulan <mhulan@redhat.com>
2014-01-28 16:19:54 +0100

    Fix dumping 'false' default values

--- lib/kafo/version.rb
+++ lib/kafo/version.rb
@@ -1,4 +1,4 @@
 # encoding: UTF-8
 module Kafo
-  VERSION = "0.3.9"
+  VERSION = "0.3.10"
 end
--- modules/kafo_configure/lib/puppet/parser/functions/dump_values.rb
+++ modules/kafo_configure/lib/puppet/parser/functions/dump_values.rb
@@ -4,7 +4,11 @@ module Puppet::Parser::Functions
   newfunction(:dump_values) do |args|
     options = []
     options<< false if Puppet::PUPPETVERSION.start_with?('2.6')
-    data = Hash[args.map { |arg| [arg, lookupvar(arg, *options) || arg] }]
+    data = args.map do |arg|
+      found_value = lookupvar(arg, *options)
+      [arg, found_value.nil? ? arg : found_value]
+    end
+    data = Hash[data]
     dump_dir = YAML.load_file(lookupvar('kafo_config_file'))[:default_values_dir]
     File.open("#{dump_dir}/default_values.yaml", 'w') { |file| file.write(YAML.dump(data)) }
   end
