e85bc6da71eff2d5a9e16f4889f45e90e89c5eb1
Nils Domrose <nils@domrose.net>
2014-02-17 13:24:34 +0100

    moved file creation into kafofy
    fixed typo
    
    removed duplicate constant, fixed static config dump

--- lib/kafo/kafo_configure.rb
+++ lib/kafo/kafo_configure.rb
@@ -354,7 +354,7 @@ module Kafo
     end
 
     def config_file
-      return CONFIG_FILE if defined?(CONFIG_FILE)
+      return CONFIG_FILE if defined?(CONFIG_FILE) && File.exists?(CONFIG_FILE)
       return '/etc/kafo/kafo.yaml' if File.exists?('/etc/kafo/kafo.yaml')
       File.join(Dir.pwd, 'config', 'kafo.yaml')
     end
