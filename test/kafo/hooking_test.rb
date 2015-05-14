e4d05a95174108a9ce66613d0d40b4f7506c2f99
John Kim <jkim@jkimdt.usersys.redhat.com>
2015-05-14 14:39:38 -0400

    Fixes #10500 - update unit test for the fix

--- test/kafo/hooking_test.rb
+++ test/kafo/hooking_test.rb
@@ -20,7 +20,9 @@ module Kafo
         KafoConfigure.logger = dummy_logger
         hooking.kafo = OpenStruct.new(:logger => dummy_logger)
         hooking.register_pre(:no1) { logger.error 's1' }
+        hooking.register_pre('str1') { logger.error 'r1' }
         hooking.register_pre(:no2) { logger.error 's2' }
+        hooking.register_pre('str2') { logger.error 'r2' }
       end
 
       # it runs in HookContext context so it has access to logger
@@ -28,7 +30,9 @@ module Kafo
         before { hooking.execute(:pre); dummy_logger.rewind }
         specify { dummy_logger.error.read.must_include 's2' }
         specify { dummy_logger.error.read.must_include 's1' }
-        specify { dummy_logger.error.read.must_match /.*s1.*s2.*/m }
+        specify { dummy_logger.error.read.must_include 'r1' }
+        specify { dummy_logger.error.read.must_include 'r2' }
+        specify { dummy_logger.error.read.must_match /.*s1.*s2.*r1.*r2.*/m }
       end
 
       describe "#execute(:post)" do
