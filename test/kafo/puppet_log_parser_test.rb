053b7c02a05679da45d5307bafe464a2a17bda91
Dominic Cleal <dominic@cleal.org>
2016-09-09 15:46:26 +0100

    fixes #16445 - send multi-line Puppet messages to last seen log level (#176)

---
+++ test/kafo/puppet_log_parser_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+module Kafo
+  describe PuppetLogParser do
+    describe "#parse" do
+      subject { PuppetLogParser.new }
+      specify { subject.parse('Error: foo').must_equal [:error, ' foo'] }
+      specify { subject.parse('Err: foo').must_equal [:error, ' foo'] }
+      specify { subject.parse('Warning: foo').must_equal [:warn, ' foo'] }
+      specify { subject.parse('Notice: foo').must_equal [:warn, ' foo'] }
+      specify { subject.parse('Debug: foo').must_equal [:debug, ' foo'] }
+      specify { subject.parse('unknown foo').must_equal [:info, 'unknown foo'] }
+      specify do
+        subject.parse('Debug: foo').must_equal [:debug, ' foo']
+        subject.parse('bar').must_equal [:debug, 'bar']
+      end
+    end
+  end
+end
