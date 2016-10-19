4293be3aba702ffa65f095307e40c5089601d3d5
Lukas Zapletal <lzap+git@redhat.com>
2016-10-19 11:35:28 +0200

    Fixes #17009 - Sort checkers to prevent race conditions

--- test/kafo/system_check_test.rb
+++ test/kafo/system_check_test.rb
@@ -2,9 +2,17 @@ require 'test_helper'
 
 module Kafo
   describe SystemChecker do
+    PATH = './test/fixtures/checks/pass'
+
+    it "returns loads checks in order" do
+      KafoConfigure.stub(:check_dirs, [PATH]) do
+        expected = ["./test/fixtures/checks/pass/pass.sh", "./test/fixtures/checks/pass/this_also_passes.sh"]
+        assert_equal expected, SystemChecker.new(File.join(PATH, '*')).checkers
+      end
+    end
 
     it "returns true if all checks pass" do
-      KafoConfigure.stub(:check_dirs, ['./test/fixtures/checks/pass']) do
+      KafoConfigure.stub(:check_dirs, [PATH]) do
         assert SystemChecker.check
       end
     end
