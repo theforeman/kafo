b00f286ef27a0ee88bb0c76b0c103b4b38e65060
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-02-20 15:52:44 +0100

    Add types to kafo_configure::dump_values

--- modules/kafo_configure/manifests/dump_values.pp
+++ modules/kafo_configure/manifests/dump_values.pp
@@ -4,7 +4,10 @@
 #  @param variables a list of variables from params classes that have
 #                   already been included
 #  @param lookups a list of variables to find through lookup()
-class kafo_configure::dump_values($variables, $lookups) {
+class kafo_configure::dump_values(
+  Array[String] $variables = [],
+  Array[String] $lookups = [],
+) {
   $dumped_vars = dump_values($variables)
   $dumped_lookups = dump_lookups($lookups)
   $dumped = foreman_to_yaml($dumped_vars, $dumped_lookups)
