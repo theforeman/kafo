theforeman/kafo

commit 29999419d8ef5288c3a87708e3cbabd896422576

Introduce an ExecutionEnvironment
The ExecutionEnvironment is a temporary directory that holds all the
relevant files during a Puppet application. This makes it easier to
clean up. It also allows easier changes to the application process.

lib/kafo/kafo_configure.rb
@@ -26,8 +26,7 @@ require 'kafo/progress_bar'
 require 'kafo/hooking'
 require 'kafo/exit_handler'
 require 'kafo/scenario_manager'
-require 'kafo/hiera_configurer'
-require 'kafo/puppet_configurer'
+require 'kafo/execution_environment'
 
 module Kafo
   class KafoConfigure < Clamp::Command
@@ -424,20 +423,17 @@ module Kafo
     def run_installation
       self.class.hooking.execute(:pre)
 
-      hiera = HieraConfigurer.new(config.app[:hiera_config], config.modules, config.app[:order])
-      hiera.write_configs
-      self.class.exit_handler.register_cleanup_path(hiera.temp_dir)
+      execution_env = ExecutionEnvironment.new(config)
+      self.class.exit_handler.register_cleanup_path(execution_env.directory)
 
-      puppetconf = PuppetConfigurer.new(
-        'color'           => false,
-        'evaltrace'       => !!@progress_bar,
-        'hiera_config'    => hiera.config_path,
-        'noop'            => !!noop?,
-        'profile'         => !!profile?,
-        'show_diff'       => true,
-        'environmentpath' => hiera.temp_dir,
+      execution_env.store_answers
+      puppetconf = execution_env.configure_puppet(
+        'color'     => false,
+        'evaltrace' => !!@progressbar,
+        'noop'      => !!noop?,
+        'profile'   => !!profile?,
+        'show_diff' => true,
       )
-      self.class.exit_handler.register_cleanup_path(puppetconf.config_path)
 
       exit_code   = 0
       exit_status = nil

doc/kafo_run.uml
@@ -94,7 +94,7 @@
 
   #Greenyellow:**pre**|
 
-  :setup HieraConfigurer and save configs;
+  :setup ExecutionEnvironment and save configs;
 
   #Gold:run puppet;
   repeat

lib/kafo/configuration.rb
@@ -4,7 +4,7 @@ require 'tmpdir'
 require 'kafo/puppet_module'
 require 'kafo/color_scheme'
 require 'kafo/data_type_parser'
-require 'kafo/puppet_configurer'
+require 'kafo/execution_environment'
 
 module Kafo
   class Configuration
@@ -148,8 +148,10 @@ module Kafo
 
     def params_default_values
       @params_default_values ||= begin
-        puppetconf = PuppetConfigurer.new('noop' => true)
-        KafoConfigure.exit_handler.register_cleanup_path puppetconf.config_path
+        execution_env = ExecutionEnvironment.new(self)
+        KafoConfigure.exit_handler.register_cleanup_path(execution_env.directory)
+
+        puppetconf = execution_env.configure_puppet('noop' => true)
 
         dump_manifest = <<EOS
           #{includes}

lib/kafo/execution_environment.rb
@@ -1,0 +1,48 @@
+require 'tmpdir'
+
+require 'kafo/hiera_configurer'
+require 'kafo/puppet_configurer'
+
+module Kafo
+  class ExecutionEnvironment
+    def initialize(config, logger = KafoConfigure.logger)
+      @config = config
+      @logger = logger
+    end
+
+    def directory
+      @directory ||= begin
+        directory = Dir.mktmpdir('kafo_installation')
+        @logger.debug("Creating execution environment in #{directory}")
+        @hiera_config_path = File.join(directory, 'hiera.conf')
+        directory
+      end
+    end
+
+    def store_answers
+      hiera = HieraConfigurer.new(@config.app[:hiera_config], @config.modules, @config.app[:order], directory)
+      @hiera_config_path = hiera.write_configs
+    end
+
+    def configure_puppet(settings = {})
+      @logger.debug("Configuring Puppet in #{directory}")
+
+      settings = {
+        'environmentpath' => environmentpath,
+        'hiera_config'    => @hiera_config_path,
+      }.merge(settings)
+
+      PuppetConfigurer.new(puppet_conf, settings)
+    end
+
+    private
+
+    def environmentpath
+      File.join(directory, 'environments')
+    end
+
+    def puppet_conf
+      File.join(directory, 'puppet.conf')
+    end
+  end
+end

lib/kafo/hiera_configurer.rb
@@ -37,6 +37,8 @@ module Kafo
       File.open(File.join(data_dir, HIERARCHY_FILENAME), 'w') do |f|
         f.write(format_yaml_symbols(generate_data(@modules).to_yaml))
       end
+
+      config_path
     end
 
     def generate_config(config = {})

lib/kafo/puppet_configurer.rb
@@ -2,16 +2,12 @@ require 'tempfile'
 
 module Kafo
   class PuppetConfigurer
-    attr_reader :logger
+    attr_reader :logger, :config_path
 
-    def initialize(settings = {})
+    def initialize(config_path, settings = {})
+      @config_path = config_path
       @settings = {'reports' => ''}.merge(settings)
       @logger = KafoConfigure.logger
-      @temp_file = Tempfile.new(['kafo_puppet', '.conf'])
-    end
-
-    def config_path
-      @temp_file.path
     end
 
     def [](key)
@@ -23,15 +19,13 @@ module Kafo
     end
 
     def write_config
-      @logger.debug("Writing Puppet config file at #{@temp_file.path}")
-      @temp_file.open
-      @temp_file.truncate(0)
-      @temp_file.puts '[main]'
-      @settings.keys.sort.each do |key|
-        @temp_file.puts "#{key} = #{@settings[key]}"
+      @logger.debug("Writing Puppet config file at #{config_path}")
+      File.open(config_path, 'w') do |file|
+        file.puts '[main]'
+        @settings.keys.sort.each do |key|
+          file.puts "#{key} = #{@settings[key]}"
+        end
       end
-    ensure
-      @temp_file.close
     end
   end
 end

test/kafo/execution_environment_test.rb
@@ -1,0 +1,24 @@
+require 'test_helper'
+
+module Kafo
+  describe ExecutionEnvironment do
+    let(:config_file) { ConfigFileFactory.build('basic', BASIC_CONFIGURATION).path }
+    let(:config) { Kafo::Configuration.new(config_file, false) }
+    let(:execution_environment) { ExecutionEnvironment.new(config) }
+
+    after { FileUtils.rm_rf(execution_environment.directory) }
+
+    describe '#directory' do
+      specify { assert File.directory?(execution_environment.directory) }
+      specify { refute File.exist?(File.join(execution_environment.directory, 'hiera.conf')) }
+    end
+
+    describe '#configure_puppet' do
+      let(:puppet_configurer) { execution_environment.configure_puppet }
+      let(:directory) { execution_environment.directory }
+      specify { assert_equal(File.join(directory, 'puppet.conf'), puppet_configurer.config_path) }
+      specify { assert_equal(File.join(directory, 'hiera.conf'), puppet_configurer['hiera_config']) }
+      specify { assert_equal(File.join(directory, 'environments'), puppet_configurer['environmentpath']) }
+    end
+  end
+end

commit ccb429e7549aa9bc9ada2cb40b69b73f88b036aa

Fixes #28827 - Use the correct progress_bar variable
29999419d8ef5288c3a87708e3cbabd896422576 introduced the execution
environment but used the incorrect variable. Since Ruby doesn't fail on
undefined variables, this silently failed.

lib/kafo/kafo_configure.rb
@@ -429,7 +429,7 @@ module Kafo
       execution_env.store_answers
       puppetconf = execution_env.configure_puppet(
         'color'     => false,
-        'evaltrace' => !!@progressbar,
+        'evaltrace' => !!@progress_bar,
         'noop'      => !!noop?,
         'profile'   => !!profile?,
         'show_diff' => true,