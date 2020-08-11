310cd998401d181fd2ec79411e205fc1497c08ad
Eric D. Helms <ericdhelms@gmail.com>
2020-08-11 19:15:11 +0000

    Ensure no leading or trailing whitespace of Puppet log output

--- test/kafo/puppet_log_parser_test.rb
+++ test/kafo/puppet_log_parser_test.rb
@@ -6,14 +6,14 @@ module Kafo
   describe PuppetLogParser do
     describe "#parse" do
       subject { PuppetLogParser.new }
-      specify { _(subject.parse('Error: foo')).must_equal [:error, ' foo'] }
-      specify { _(subject.parse('Err: foo')).must_equal [:error, ' foo'] }
-      specify { _(subject.parse('Warning: foo')).must_equal [:warn, ' foo'] }
-      specify { _(subject.parse('Notice: foo')).must_equal [:warn, ' foo'] }
-      specify { _(subject.parse('Debug: foo')).must_equal [:debug, ' foo'] }
+      specify { _(subject.parse('Error: foo')).must_equal [:error, 'foo'] }
+      specify { _(subject.parse('Err: foo')).must_equal [:error, 'foo'] }
+      specify { _(subject.parse('Warning: foo')).must_equal [:warn, 'foo'] }
+      specify { _(subject.parse('Notice: foo')).must_equal [:warn, 'foo'] }
+      specify { _(subject.parse('Debug: foo')).must_equal [:debug, 'foo'] }
       specify { _(subject.parse('unknown foo')).must_equal [:info, 'unknown foo'] }
       specify do
-        _(subject.parse('Debug: foo')).must_equal [:debug, ' foo']
+        _(subject.parse('Debug: foo')).must_equal [:debug, 'foo']
         _(subject.parse('bar')).must_equal [:debug, 'bar']
       end
     end
