45aee7fb0c9009e844d1673f1e836fe4362af12b
Eric D. Helms <ericdhelms@gmail.com>
2020-10-24 21:31:03 +0000

    Add ability to specify an option as advanced
    Gives built in options as well as hook's the ability to specify
    that an option is advanced and thereby present the option only
    under the --full-help menu.

--- lib/kafo/app_option/definition.rb
+++ lib/kafo/app_option/definition.rb
@@ -3,7 +3,12 @@ module Kafo
     class Definition < Clamp::Option::Definition
 
       def initialize(switches, type, description, options = {})
+        @advanced = options.fetch(:advanced, false)
         super(switches, type, description, options)
+      end
+
+      def advanced?
+        @advanced
       end
 
     end
--- lib/kafo/help_builders/basic.rb
+++ lib/kafo/help_builders/basic.rb
@@ -4,8 +4,15 @@ module Kafo
   module HelpBuilders
     class Basic < Base
       def add_module(name, items)
-        data = by_parameter_groups(except_resets(items))
-        add_list(module_header(name), data['Basic'])
+        pruned = except_resets(items)
+        pruned = except_advanced(pruned)
+        data = by_parameter_groups(pruned)
+        add_list(module_header(name), data[DEFAULT_GROUP_NAME])
+      end
+
+      def add_list(heading, items)
+        pruned = except_advanced(items)
+        super(heading, pruned)
       end
 
       def string
@@ -16,6 +23,10 @@ module Kafo
 
       def except_resets(items)
         items.select { |i| !i.help.first.strip.start_with?('--reset-') || !i.help.last.include?('to the default value (') }
+      end
+
+      def except_advanced(items)
+        items.reject { |item| item.respond_to?(:advanced?) && item.advanced? }
       end
     end
   end
