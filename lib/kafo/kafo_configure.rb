theforeman/kafo

commit 0612410091e7c74f8493fbaec1df9a516451018a

Make Puppet reports available from the ExecEnv
Puppet produces reports. These can be used to produce good instructions
to users about what exactly failed.

lib/kafo/kafo_configure.rb
@@ -20,8 +20,10 @@ require 'kafo/string_helper'
 require 'kafo/help_builder'
 require 'kafo/wizard'
 require 'kafo/system_checker'
+require 'kafo/failed_puppet_resource'
 require 'kafo/puppet_command'
 require 'kafo/puppet_log_parser'
+require 'kafo/puppet_report'
 require 'kafo/progress_bar'
 require 'kafo/hooking'
 require 'kafo/exit_handler'
@@ -39,7 +41,8 @@ module Kafo
 
       attr_accessor :config, :root_dir, :config_file, :gem_root,
                     :module_dirs, :kafo_modules_dir, :verbose, :logger,
-                    :check_dirs, :exit_handler, :scenario_manager, :store
+                    :check_dirs, :exit_handler, :scenario_manager, :store,
+                    :puppet_report
       attr_writer :hooking
 
       def hooking
@@ -221,6 +224,10 @@ module Kafo
 
     def exit_code
       self.class.exit_code
+    end
+
+    def puppet_report
+      self.class.puppet_report
     end
 
     def help
@@ -541,6 +548,12 @@ module Kafo
 
       @progress_bar.close if @progress_bar
       logger.notice "System configuration has finished."
+
+      if (last_report = execution_env.reports.last)
+        # For debugging: you can easily copy the last report to fixtures
+        # FileUtils.cp(last_report, File.join(__dir__, '..', '..', 'test', 'fixtures', 'reports', File.basename(last_report)))
+        self.class.puppet_report = PuppetReport.load_report_file(last_report)
+      end
 
       self.class.hooking.execute(:post)
       self.class.exit(exit_code)

test/kafo/kafo_configure_test.rb
@@ -29,5 +29,23 @@ module Kafo
         _(KafoConfigure.use_colors?).must_equal false
       end
     end
+
+    describe '#puppet_report' do
+
+      before { KafoConfigure.puppet_report = report }
+      after { KafoConfigure.puppet_report = nil }
+
+      describe 'without a report' do
+        let(:report) { nil }
+
+        specify { assert_nil KafoConfigure.puppet_report }
+      end
+
+      describe 'with a report' do
+        let(:report) { PuppetReport.new({ 'report_format' => 11 }) }
+
+        specify { assert_equal(report, KafoConfigure.puppet_report) }
+      end
+    end
   end
 end

test/acceptance/kafo_configure_test.rb
@@ -341,5 +341,36 @@ module Kafo
         _(stdout).must_include "Hello Kafo\nGoodbye"
       end
     end
+
+    describe 'puppet_report hook context' do
+      it 'has a Puppet report format' do
+        _code, stdout, _stderr = run_command '../bin/kafo-configure'
+
+        assert_match(/Puppet report format: \d+$/, stdout)
+      end
+    end
+
+    describe 'with failure module' do
+      before { add_manifest('failure') }
+
+      it 'triggers a failure' do
+        process, stdout, stderr = run_command('../bin/kafo-configure --testing-fail exec')
+
+        expected_stdout = <<~EXPECTED
+        Puppet Exec resource 'failing-command' failed. Logs:
+        require to File[#{TMPDIR}/failing-command]
+        Starting to evaluate the resource (12 of 21)
+        Executing '#{TMPDIR}/failing-command'
+        This is stdout
+        This is stderr
+        change from 'notrun' to ['0'] failed: '#{TMPDIR}/failing-command' returned 100 instead of one of [0]
+        Evaluated in 0.01 seconds
+        EXPECTED
+
+        assert_includes(stderr, "failed: '#{TMPDIR}/failing-command' returned 100")
+        assert_includes(stdout, expected_stdout)
+        assert_equal(6, process.exitstatus)
+      end
+    end
   end
 end

lib/kafo/exceptions.rb
@@ -8,4 +8,7 @@ module Kafo
 
   class ParserError < StandardError
   end
+
+  class PuppetReportError < StandardError
+  end
 end

lib/kafo/execution_environment.rb
@@ -19,6 +19,15 @@ module Kafo
       end
     end
 
+    def reportdir
+      @reportdir ||= File.join(directory, 'reports')
+    end
+
+    def reports
+      # Reports are stored in $reportdir/$certname/$report
+      Dir.glob(File.join(reportdir, '*', '*.*')).sort_by { |path| File.mtime(path) }
+    end
+
     def store_answers
       answer_data = HieraConfigurer.generate_data(@config.modules, @config.app[:order])
       @logger.debug("Writing temporary answers to #{answer_file}")
@@ -37,6 +46,8 @@ module Kafo
         'environmentpath' => environmentpath,
         'factpath'        => factpath,
         'hiera_config'    => hiera_config,
+        'reports'         => 'store',
+        'reportdir'       => reportdir,
       }.merge(settings)
 
       PuppetConfigurer.new(puppet_conf, settings)

lib/kafo/failed_puppet_resource.rb
@@ -1,0 +1,53 @@
+module Kafo
+  class PuppetFailedResource
+    # @param [Hash] status
+    #   The status hash from the report
+    # @param [Array[Hash]] logs
+    #   Relevant log lines for this resoure
+    def initialize(status, logs)
+      @status = status
+      @logs = logs
+    end
+
+    # @example
+    #   puppet_failed_resource.resource == 'Exec[/bin/true]'
+    # @return [String] A resource
+    def resource
+      @status['resource']
+    end
+
+    # @example
+    #   puppet_failed_resource.type == 'Exec'
+    # @return [String] A resource type
+    def type
+      @status['resource_type']
+    end
+
+    # @example
+    #   puppet_failed_resource.title == '/bin/true'
+    # @return [String] A resource title
+    def title
+      @status['title']
+    end
+
+    def to_s
+      "Puppet #{type} resource '#{title}'"
+    end
+
+    # @return [Array[String]] The event messages
+    def event_messages
+      @status['events'].map { |event| event['message'] }
+    end
+
+    # A collection of Puppet log messages
+    #
+    # The log messages include detailed information of what failed. Some debug
+    # information, such as timing but crucially the command output, both stdout
+    # and stderr.
+    #
+    # @return [Array[String]] The Puppet log messages for this resource
+    def log_messages
+      @logs.map { |log| log['message'] }
+    end
+  end
+end

lib/kafo/hook_context.rb
@@ -196,5 +196,13 @@ module Kafo
     def exit_code
       self.kafo.exit_code
     end
+
+    # Return the Puppet report, if any.
+    # Only available after Puppet actual ran.
+    #
+    # @return [Optional[Kafo::PuppetReport]]
+    def puppet_report
+      self.kafo.puppet_report
+    end
   end
 end

lib/kafo/puppet_report.rb
@@ -1,0 +1,64 @@
+module Kafo
+  # An abstraction over the Puppet report format
+  #
+  # @see https://puppet.com/docs/puppet/8/format_report.html
+  class PuppetReport
+    # Load a Puppet report from a path
+    #
+    # Both YAML and JSON are supported.
+    #
+    # @param [String] path
+    #   The path to Puppet report
+    #
+    # @return [PuppetReport] The report from the path
+    def self.load_report_file(path)
+      raise ArgumentError, 'No path given' unless path || path.empty?
+      raise ArgumentError, "#{path} is not a readable file" unless File.file?(path) && File.readable?(path)
+
+      data = case File.extname(path)
+             when '.yaml'
+               require 'yaml'
+               content = File.read(path).gsub(/\!ruby\/object.*$/, '')
+               YAML.safe_load(content, permitted_classes: [Time, Symbol])
+             when '.json'
+               require 'json'
+               JSON.parse(File.read(path))
+             else
+               raise ArgumentError, "Unsupported file extension for #{path}"
+             end
+
+      PuppetReport.new(data)
+    end
+
+    # @param [Hash] report
+    #   The Puppet report
+    def initialize(report)
+      @report = report
+    end
+
+    # @return [Integer] The report format
+    def report_format
+      @report['report_format']
+    end
+
+    # @return [Array[Hash]] The Puppet logs
+    def logs
+      @report['logs']
+    end
+
+    # @return [Array[PuppetFailedResource]] The failed resources and their status
+    def failed_resources
+      statuses = @report['resource_statuses']
+
+      raise PuppetReportError, "No resource statuses found in report" unless statuses
+
+      statuses.select { |_title, status| status['failed'] }.map do |title, status|
+        # TODO: There's also a message with source Puppet
+        # Executing with uid=USER: '/tmp/failing-command'
+        # This shows up after Executing '/tmp/failing-command'
+        related_logs = logs.select { |log| log['source'].include?(title) }
+        PuppetFailedResource.new(status, related_logs)
+      end
+    end
+  end
+end

commit fb181adafda927ea7a6f07797495f9ceb7fd415c

Store puppet_report as an instance variable
Somehow it wasn't working and storing it as an instance variable is
cleaner anyway.

Fixes: 0612410091e7c74f8493fbaec1df9a516451018a ("Make Puppet reports available from the ExecEnv")

lib/kafo/kafo_configure.rb
@@ -36,13 +36,14 @@ module Kafo
   class KafoConfigure < Clamp::Command
     include StringHelper
 
+    attr_accessor :puppet_report
+
     class << self
       include AppOption::Declaration
 
       attr_accessor :config, :root_dir, :config_file, :gem_root,
                     :module_dirs, :kafo_modules_dir, :verbose, :logger,
-                    :check_dirs, :exit_handler, :scenario_manager, :store,
-                    :puppet_report
+                    :check_dirs, :exit_handler, :scenario_manager, :store
       attr_writer :hooking
 
       def hooking
@@ -224,10 +225,6 @@ module Kafo
 
     def exit_code
       self.class.exit_code
-    end
-
-    def puppet_report
-      self.class.puppet_report
     end
 
     def help
@@ -552,7 +549,7 @@ module Kafo
       if (last_report = execution_env.reports.last)
         # For debugging: you can easily copy the last report to fixtures
         # FileUtils.cp(last_report, File.join(__dir__, '..', '..', 'test', 'fixtures', 'reports', File.basename(last_report)))
-        self.class.puppet_report = PuppetReport.load_report_file(last_report)
+        self.puppet_report = PuppetReport.load_report_file(last_report)
       end
 
       self.class.hooking.execute(:post)

test/kafo/kafo_configure_test.rb
@@ -29,23 +29,5 @@ module Kafo
         _(KafoConfigure.use_colors?).must_equal false
       end
     end
-
-    describe '#puppet_report' do
-
-      before { KafoConfigure.puppet_report = report }
-      after { KafoConfigure.puppet_report = nil }
-
-      describe 'without a report' do
-        let(:report) { nil }
-
-        specify { assert_nil KafoConfigure.puppet_report }
-      end
-
-      describe 'with a report' do
-        let(:report) { PuppetReport.new({ 'report_format' => 11 }) }
-
-        specify { assert_equal(report, KafoConfigure.puppet_report) }
-      end
-    end
   end
 end