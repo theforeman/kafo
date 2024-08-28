theforeman/kafo

commit 86f29674479c4c5634a892dcc79a06f75e635786

fixes #19215 - parse nested data type arguments accurately (#212)
No longer greedily matches data type arguments until the last closing
square bracket, ensuring arguments are split correctly. Changed to a
string scanning implementation so it's able to parse nested brackets
without relying on the greedy regexp (or recursion).

lib/kafo/data_type.rb
@@ -1,3 +1,5 @@
+require 'strscan'
+
 module Kafo
   class DataType
     def self.new_from_string(str)
@@ -39,16 +41,42 @@ module Kafo
     end
 
     def self.split_arguments(input)
-      input.scan(%r{
-        \s*
-          (?:
-            ["'/](.*?)["'/] # quoted string, or regexp argument
-            |
-            ([\w:]+ (?:\[.+\])?) # bare words, or Type::Name, or Type::Name[args..]
-          )
-        \s*
-        (?:,|$) # match to end of comma-separated arg, or the last arg
-      }mx).flatten.compact
+      scanner = StringScanner.new(input)
+      args = []
+
+      until scanner.eos?
+        scanner.skip(/\s*/)
+
+        if %w(' " /).include?(quote = scanner.peek(1)) # quoted string, or regexp argument
+          scanner.getch  # skip first quote
+          quoted = scanner.scan_until(/(?:^|[^\\])#{quote}/) or raise ConfigurationException, "missing end quote in argument #{args.count + 1} in data type #{input}"
+          args << quoted[0..-2]  # store unquoted value
+
+        else # bare words, or Type::Name, or Type::Name[args..]
+          type = scanner.scan(/[\w:-]+/) or raise ConfigurationException, "missing argument #{args.count + 1} to data type #{input}"#
+
+          # store inner arguments as a continuation of the type string
+          if scanner.peek(1) == '['
+            type << scanner.getch
+            bracket_count = 1
+            until bracket_count.zero?
+              next_bracket = scanner.scan_until(/[\[\]]/) or raise ConfigurationException, "missing close bracket in argument #{args.count + 1} in data type #{input}"
+              case next_bracket[-1..-1]
+              when '['
+                bracket_count += 1
+              when ']'
+                bracket_count -= 1
+              end
+              type << next_bracket
+            end
+          end
+          args << type
+        end
+
+        scanner.skip(/\s*,?/)
+      end
+
+      args
     end
 
     def self.parse_hash(input)

test/kafo/data_type_test.rb
@@ -58,8 +58,23 @@ module Kafo
       end
 
       it 'instantiates type with multiple arguments' do
-        data_type.expect(:new, 'instance', ['1', 'Float', '(regexp)', 'Enum["foo", \'bar\']', '2'])
-        DataType.new_from_string('Example[1,Float, /(regexp)/, Enum["foo", \'bar\'],2]').must_equal 'instance'
+        data_type.expect(:new, 'instance', ['1', 'Float', '(regexp)', 'Enum["foo", \'bar\']', '-2'])
+        DataType.new_from_string('Example[1,Float, /(regexp)/, Enum["foo", \'bar\'],-2]').must_equal 'instance'
+      end
+
+      it 'instantiates type with multiple nested arguments' do
+        data_type.expect(:new, 'instance', ['Hash[String, String]', 'Hash[String, String]'])
+        DataType.new_from_string('Example[Hash[String, String], Hash[String, String]]').must_equal 'instance'
+      end
+
+      it 'instantiates type with multiple nested arguments (2 levels)' do
+        data_type.expect(:new, 'instance', ['Hash[Array[String], Array[String]]', 'Hash[Array[String], Array[String]]'])
+        DataType.new_from_string('Example[Hash[Array[String], Array[String]], Hash[Array[String], Array[String]]]').must_equal 'instance'
+      end
+
+      it 'instantiates type with escaped regexes in arguments' do
+        data_type.expect(:new, 'instance', ['https?:\/\/\w+\.com', 'String'])
+        DataType.new_from_string('Example[/https?:\/\/\w+\.com/, String]').must_equal 'instance'
       end
 
       it 'instantiates type with hash arguments' do
@@ -69,6 +84,18 @@ module Kafo
         DataType.new_from_string('Example[{mode            => Enum[read, write, update],
                                            path            => Optional[String[1]],
                                            NotUndef[owner] => Optional[String[1]]}]').must_equal 'instance'
+      end
+
+      it 'raises error parsing types with mismatched quotes' do
+        Proc.new { DataType.new_from_string('Example["test]') }.must_raise ConfigurationException
+      end
+
+      it 'raises error parsing types with mismatched brackets' do
+        Proc.new { DataType.new_from_string('Example[Array[String]') }.must_raise ConfigurationException
+      end
+
+      it 'raises error parsing types with unknown argument' do
+        Proc.new { DataType.new_from_string('Example[&]') }.must_raise ConfigurationException
       end
 
       it 'aliases Data to Any' do

commit eaad6c5e8e51f70ea5f57dfd352cc9e6bf60954a

drop stray comment sign to make rubocop happy again
Fixes: 86f29674479c4c5634a892dcc79a06f75e635786

lib/kafo/data_type.rb
@@ -53,7 +53,7 @@ module Kafo
           args << quoted[0..-2]  # store unquoted value
 
         else # bare words, or Type::Name, or Type::Name[args..]
-          type = scanner.scan(/[\w:-]+/) or raise ConfigurationException, "missing argument #{args.count + 1} to data type #{input}"#
+          type = scanner.scan(/[\w:-]+/) or raise ConfigurationException, "missing argument #{args.count + 1} to data type #{input}"
 
           # store inner arguments as a continuation of the type string
           if scanner.peek(1) == '['